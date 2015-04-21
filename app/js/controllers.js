3bdf2805402ae1bf04adfd9ff2f3343cfe37e73a
Lesia Nalepa <lesia.nalepa@gmail.com>
2015-04-20 22:35:23 -0400

    recipe detail page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,21 @@ var recipeManagerControllers = angular.module('recipeManagerControllers', []);
 
 recipeManagerControllers.controller('RecipesCtrl', ['$scope', '$http',
   function($scope, $http) {
-  	$scope.recipes = [
-        {'name': 'Banana Bread',
-         'date': '05/29/2014'},
-        {'name': 'Mac n Cheese',
-         'date': '09/13/2014'},
-        {'name': 'Lasagna',
-         'date': '06/14/2014'}
+      $scope.recipes = [
+        {'id':'1',
+         'tags':['dessert','fruit'],
+         'ingredients':[{qty: 4, units: 'whole', ingredient: 'bananas'}, {qty: 1.5, units: 'cups', ingredient: 'flour'}],
+         'added':'05/29/2014',
+         'title':'Banana Bread',
+         'time':1,
+         'instructions':'Mash bananas. Add flour. Bake.'},
+        {'id':'2',
+         'tags':['mexican','snack'],
+         'ingredients':[{qty: 3, units: 'whole', ingredient: 'avacados'}, {qty: 1, units: 'whole', ingredient: 'limes'}],
+         'added':'04/13/2014',
+         'title':'Guacamole',
+         'time':0,
+         'instructions':'Mash avacado, add lime. Serve.'} 
       ];
     // $http.get('recipes/recipes.json').success(function(data) {
     //   $scope.recipes = data;
@@ -19,6 +27,26 @@ recipeManagerControllers.controller('RecipesCtrl', ['$scope', '$http',
 
 recipeManagerControllers.controller('RecipeDetailCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
+    
     $scope.recipeId = $routeParams.recipeId;
+
+    var recipes = [
+        {'id':'1',
+         'tags':['dessert','fruit'],
+         'ingredients':[{qty: 4, units: 'whole', ingredient: 'bananas'}, {qty: 1.5, units: 'cups', ingredient: 'flour'}],
+         'added':'05/29/2014',
+         'title':'Banana Bread',
+         'time':1,
+         'instructions':'Mash bananas. Add flour. Bake.'},
+        {'id':'2',
+         'tags':['mexican','snack'],
+         'ingredients':[{qty: 3, units: 'whole', ingredient: 'avacados'}, {qty: 1, units: 'whole', ingredient: 'limes'}],
+         'added':'04/13/2014',
+         'title':'Guacamole',
+         'time':0,
+         'instructions':'Mash avacado, add lime. Serve.'} 
+      ];
+
+     $scope.recipe = recipes[$scope.recipeId-1];
   }]);
 
